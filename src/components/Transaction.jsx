0b8c27205e32e9fa7b19198ba28f016ac984a38c
khaled308 <khaledgaballa4@gmail.com>
2022-02-14 13:05:01 +0200

    update some code

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,32 +1,35 @@
 import { useContext } from 'react/'
 import Context from '../Context'
+import { useState } from "react"
 
 function Transaction() {
     const{income,expense,history} = useContext(Context)
     const [incomeTrans,setIncome] = income
     const [expenseTrans,setExpense] = expense
     const [historyTrack , setHistory] = history
+    const [amount,setAmount] = useState('')
+    const [description,setDescription] = useState('')
     function AddTransaction(e){
         e.preventDefault()
-        const fields = document.querySelectorAll('form input')
-        if(fields[1].value.trim().length === 0 || fields[1].value.trim().length === 0  || isNaN(+fields[1].value)){
+        if(amount.length === 0 || description.length === 0  || isNaN(+amount)){
             return
         }
-        else if(+fields[1].value > 0){
-            setIncome( incomeTrans + +fields[1].value )
+        else if(+amount > 0){
+            setIncome( incomeTrans + +amount )
         }
         else{
-            setExpense(expenseTrans + +fields[1].value*-1)
+            setExpense(expenseTrans + +amount*-1)
         }
-        setHistory([...historyTrack,{description: fields[0].value , amount: fields[1].value}])
-        fields.forEach(field=>field.value = '')
+        setHistory([...historyTrack,{description , amount}])
+        setAmount('')
+        setDescription('')
     }
     return (
         <div className="transaction">
             <form>
                 <h2>Add New Transaction</h2>
-                Description <input type="text" />
-                Transaction Amount <input type="text"/>
+                Description <input type="text" value={description} onChange={(e)=>setDescription(e.target.value)} />
+                Transaction Amount <input type="text" value={amount} onChange={(e)=>setAmount(e.target.value)}/>
                 <button onClick={AddTransaction}>Add Transaction</button>
             </form>
         </div>
